e0513b028917aaf0bd699d1fbf18f51a25d0e75a
romart008 <martunykr9@gmail.com>
2025-06-09 18:37:44 +0300

    Complete reconstruction. Hiding Message. Times more quick

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # Steganography project
 #
-# 1. Use every bit of file
-# 2. Use root of number to encode a message in specific way
+# 1. Use root of number to encode a message in specific way
+# 2. Add GUI
 #
 # ! OPTIMIZE
 
 from PIL import Image
 import numpy as np
-
-Matrix = []
-
 
 def open_image(image_path):
     img = Image.open(image_path).convert("RGB")  # Opening image as rgb
@@ -17,7 +14,6 @@ def open_image(image_path):
     width, height = img.size
 
     Cover = []
-    bits = []
 
     for r, g, b in pixels:
         t1 = [int(x) for x in format(r, '08b')]
@@ -25,103 +21,102 @@ def open_image(image_path):
         t3 = [int(x) for x in format(b, '08b')]
 
         Cover.extend([t1, t2, t3])               # Data of the image
-        bits.extend([t1[-1], t2[-1], t3[-1]])     # Last bits
 
-    return np.array(Cover), np.array(bits), width, height
+    return np.array(Cover), width, height
 
+def convert(msg, p):
+    new_msg = []
+    res = ''.join(format(ord(i), '08b') for i in msg)
+    l = len(res)
 
-def Message(msg):
-    msg_b = ''.join(format(x, '08b') for x in bytearray(msg, 'utf-8'))  # Converting msg to binary
-    p = len(msg_b)
-    return msg_b, p, 2**p - 1  # 2**p - 1 - length of matrix for hiding
+    for i in range(l//p):
+        new_msg.append(res[i*p:i*p+p])
+    if l%p != 0:
+        new_msg.append(res[(l//p)*p:])
+        for i in range(p - len(new_msg[-1])):
+            new_msg[-1] += '0'
 
+    return new_msg
 
-def Mat(p, l, n, t):
-    global Matrix
+def build_matrix(p):
+    return np.array([
+        [int(x) for x in format(i, f'0{p}b')]
+        for i in range(1, 2**p)
+    ])  # Transpose to get shape (p x 2^p - 1)
 
-    if n == p:
-        Matrix.append(t.copy())  # If column of p elements ready - Add to Matrix
-        return
-
-    t.append(0)
-    Mat(p, l, n + 1, t)  # Recursion
-    t.pop()
-
-    t.append(1)
-    Mat(p, l, n + 1, t)
-    t.pop()
-
-
-def transform(bits, Matrix, msg_b, width, height):
+def change(Mat, cov_elem, msg, i, d):
     global Cover
+    l = 2**p-1
 
-    msg_b = np.array([int(x) for x in msg_b])  # Converting to numpy
-
-    stego = (Matrix.T.dot([int(b) for b in bits[:len(Matrix)]]) - msg_b) % 2  # Stego vector
-
-    # --- Changing that pixel ---
-    for pos, i in enumerate(Matrix):
-        if np.array_equal(i, stego):
-            Cover[pos] = (Cover[pos] + 1) % 2
+    Mat.shape = (p, l)                                                          # Aligning matrix for correct multiplication
+    cov_elem.shape = (l)
+    vector = (Mat.dot(cov_elem) - np.array([int(b) for b in msg[i]])) % 2       # Getting the vector
+    pos = 0
+    for j in Mat.T:
+        if np.array_equal(vector, j):
+            Cover[i*l+pos][-d] = (Cover[i*l+pos][-d]+1)%2
             break
+        pos +=1
 
-    # --- Reconstructing the image ---
+
+def hide(msg, cov, p, depth):
+    Mat = build_matrix(p)                             # Creating the matrix
+    l = 2**p-1                                      # lengh of Matrix and all possible variations
+
+    
+    for i in range(int(len(msg))):                                      
+        block = np.array([s[-depth:] for s in cov[i*l:i*l+l]])                  # Creating Cover 
+
+        # !!! This method allows to hide [depth] times more information in one block, than just LSB, 
+        # !!! but the risk of discovering message rises exponentionaly
+        if depth >1:                                                    # In case we use more than last bit
+            for d in range(depth):
+                msg_idx = i * depth + d                                 # Iteration, aka how many times we alredy perfomed hiding 
+                if msg_idx >= len(msg):
+                    break
+
+                cov_elem = np.array([s[-(d + 1)] for s in block])
+                change(Mat, cov_elem, msg, msg_idx, d + 1)              
+
+        else:
+            change(Mat, block, msg, i, depth)
+
+
+def compile(Cover, width, height):
     new_image = []
-    for i in range(len(Cover) // 3):  # Converting back to base 10
-        r_bin, g_bin, b_bin = Cover[i * 3:i * 3 + 3]
+    for i in range(int(len(Cover)/3)):                       # Converting back to base 10
+        r_bin, g_bin, b_bin = Cover[i*3:i*3+3]
         r = int("".join(map(str, r_bin)), 2)
         g = int("".join(map(str, g_bin)), 2)
         b = int("".join(map(str, b_bin)), 2)
         new_image.append((r, g, b))
 
-    new_img = Image.new("RGB", (width, height))  # Recreating Image
+    new_img = Image.new("RGB", (width, height))             # Recreating Image
     new_img.putdata(new_image)
     new_img.save("output.png")
 
-
-def extract(image_path):
-    global Matrix
-
-    img = Image.open(image_path).convert("RGB")  # Opening image as rgb
-    pixels = list(img.getdata())                 # Getting all info about pixels as list
-
-    bits = []
-    for r, g, b in pixels:
-        t1 = [int(x) for x in format(r, '08b')]
-        t2 = [int(x) for x in format(g, '08b')]
-        t3 = [int(x) for x in format(b, '08b')]
-        bits.extend([t1[-1], t2[-1], t3[-1]])  # Last bits
-
-    n = 16
-    Mat(n, 2**n - 1, 0, [])
-    del Matrix[0]
-    Matrix = np.array(Matrix)
-
-    s = [int(b) for b in bits[:len(Matrix)]]
-    msg = np.dot(Matrix.T, s) % 2
-
-    msg = "".join(map(str, msg))
-    print("Message is:")
-    print(''.join(chr(int(msg[i:i + 8], 2)) for i in range(0, len(msg), 8)))
 
 
 # --- Entry Point ---
 if __name__ == "__main__":
     n = 0
+    depth = 1               # The ammount of last bites taken to change, recommended - 1, aka LSB
+    p = 4                   # Ammount of bits we hide per some space
     if n == 0:
-        Cover, bits, width, height = open_image("testimage.png")
-        msg_b, p, l = Message("Hi")
+        Cover, width, height = open_image("testimage.png")
+        print("Image opened")
 
-        Mat(p, l, 0, [])
-        del Matrix[0]
-        Matrix = np.array(Matrix)
+        Message = convert("Hello", p)
+        print("Message converted")
 
-        if len(Cover) > l:
-            transform(bits, Matrix, msg_b, width, height)
-        else:
-            print("Message too big for file")
-    else:
-        extract("output.png")
+        hide(Message, Cover, p, depth)
+        print("Message hiden")
+
+        compile(Cover, width, height)
+        print("Image Created. Done")
+
+
+        
 
 """
 ⠀⠀⠀⠀   ⠀⢀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
